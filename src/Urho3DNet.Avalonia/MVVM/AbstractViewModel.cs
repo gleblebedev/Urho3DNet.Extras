54d51fd0763c9ba22fd8c11fc10a646fdcc87c6b
Gleb Lebedev <gleb@gleblebedev.com>
2021-04-24 14:52:27 +0100

    MVVM helpers

---
+++ src/Urho3DNet.Avalonia/MVVM/AbstractViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using Avalonia.Input;
+
+namespace Urho3DNet.MVVM
+{
+    public abstract class AbstractViewModel: INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">Changed property name.</param>
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            var eventHandler = PropertyChanged;
+
+            eventHandler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
