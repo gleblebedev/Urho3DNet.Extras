86b6e15f792a9f322fb336bd1af2d5a20eee0108
Gleb Lebedev <gleb@gleblebedev.com>
2021-05-09 15:10:46 +0100

    Avalonia samples updated

--- src/Urho3DNet.SampleApp/AvaloniaSample.cs
+++ src/Urho3DNet.SampleApp/AvaloniaSample.cs
@@ -1,11 +1,11 @@
-﻿using Urho3DNet.InputEvents;
+﻿using Avalonia;
+using Urho3DNet.InputEvents;
 using Urho3DNet.SampleApp.View;
 
 namespace Urho3DNet.Samples
 {
     public class AvaloniaSample : Sample
     {
-        private readonly SharedPtr<AvaloniaElement> _sprite;
         private readonly SampleAvaloniaWindow _window;
 
         public AvaloniaSample(Context context) : base(context)
@@ -13,10 +13,7 @@ namespace Urho3DNet.Samples
             _window = new SampleAvaloniaWindow();
             _window.Width = 960;
             _window.Height = 63;
-            _window.Show();
-            //_window.InvalidateVisual();
-            _sprite = UIRoot.CreateChild<AvaloniaElement>();
-            _sprite.Value.Canvas = _window;
+            _window.Show(UIRoot);
             DefaultFogColor = new Color(0.1f, 0.2f, 0.4f, 1.0f);
             IsMouseVisible = true;
             MouseMode = MouseMode.MmFree;
@@ -30,7 +27,7 @@ namespace Urho3DNet.Samples
 
         protected override void Dispose(bool disposing)
         {
-            _sprite.Dispose();
+            _window.Close();
             base.Dispose(disposing);
         }
 
@@ -38,10 +35,11 @@ namespace Urho3DNet.Samples
         {
             base.OnResize(graphicsSize);
 
-            var avaloniaElement = _sprite.Value;
-            avaloniaElement.Size = new IntVector2(Graphics.Width/2, Graphics.Height/16);
-            avaloniaElement.Position = new Vector2(0.5f * (Graphics.Width - avaloniaElement.Width),
-                0.5f * (Graphics.Height - avaloniaElement.Height));
+            var avaloniaElement = _window;
+            avaloniaElement.Width = Graphics.Width/2;
+            avaloniaElement.Height =  Graphics.Height/16;
+            avaloniaElement.Position = new PixelPoint((int)(Graphics.Width - avaloniaElement.Width)/2,
+                (int)(Graphics.Height - avaloniaElement.Height)/2);
 
         }
     }
